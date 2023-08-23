2b9db05be3569295ebbca4b272895b5ba6c283a2
0:) <v-lvkdl@naver.com>
2023-08-23 13:57:37 +0900

    0823 add some css

--- js/weather.js
+++ js/weather.js
@@ -10,9 +10,9 @@ function onGeoOk(position){
     .then((response) => response.json())
     .then((data) => {
         const weather = document.querySelector("#weather span:first-child");
+        weather.innerText = `${data.weather[0].main}, ${data.main.temp}`;
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main}, current temp is ${data.main.temp}'c`;
     });
 };
 
