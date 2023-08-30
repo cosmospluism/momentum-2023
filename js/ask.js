cbda502ab0b4ce634e5cba8e4cd72e60d725312f
0:) <v-lvkdl@naver.com>
2023-08-30 14:52:15 +0900

    0830

--- js/ask.js
+++ js/ask.js
@@ -3,13 +3,14 @@ const todoQ = todoForm.querySelector("span");
 const todoInput = todoForm.querySelector("input");
 const answer = todoForm.querySelector(".answer");
 
-const saveTodo = localStorage.getItem("todoText");
+let saveTodo = localStorage.getItem("todoText");
 
 function onSubmit(event){
     event.preventDefault();
     const todoText = todoInput.value;
     localStorage.setItem("todoText", todoText);
     todoQ.innerText = "TODAY";
+    todoInput.value = "";
     todoInput.classList.add("hidden");
     appearTodo(todoText);
 }
@@ -20,16 +21,24 @@ function appearTodo(pizza) {
     const removeBtn = document.createElement("button");
     removeBtn.innerText = "x";
     answer.appendChild(removeBtn);
+    removeBtn.addEventListener("click", deleteTodo);
+}
+
+function deleteTodo() {
+    // const removeOne = event.target.parentElement;
+    // removeOne.remove();
+    // todoQ.innerText = "What is your plan for today?";
+    // answer.classList.add("hidden");
+    // todoInput.classList.remove("hidden");
 }
     
 if(saveTodo === null) {
-    todoQ.innerText = "What is your plan for today?";
+    // todoQ.innerText = "What is your plan for today?";
+    // todoInput.classList.remove("hidden");
     answer.classList.add("hidden");
-    todoInput.classList.remove("hidden");
     todoForm.addEventListener("submit", onSubmit);
 } else {
     todoInput.classList.add("hidden");
+    todoQ.innerText = "TODAY";
     appearTodo(saveTodo);
 }
-
-todoForm.addEventListener("submit", onSubmit);
